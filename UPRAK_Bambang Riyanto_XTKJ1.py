254740e262c061f4b7b2ac332df18a5fa651beff
ceboxskuy <110508821+ceboxskuy@users.noreply.github.com>
2022-08-11 20:37:19 -0700

    Add files via upload

---
+++ UPRAK_Bambang Riyanto_XTKJ1.py
@@ -0,0 +1,9 @@
+nama_depan = input("masukan nama depan: ")
+nama_belakang = input("masukan nama belakang: ")
+
+print("masukan nama anda", nama_depan, nama_belakang)
+
+celcius = int(input("masukan suhu celcius:"))
+ream = (4/5 * celcius)
+
+print(celcius, "celcius =", ream, "reamur")
